84c6ddf77f2a1bfb49723c19541eff6d9c339e4e
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-08-26 17:22:22 -0300

    Adding rspec tests to BooksController

---
+++ spec/controllers/books_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe BooksController, type: :controller do
+
+  let(:b) { FactoryGirl.create(:book) }
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, id: b
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit, id: b
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
