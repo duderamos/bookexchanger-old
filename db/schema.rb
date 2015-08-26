cbaccd7172d76a14987c436e50ddf72ef2aa6ac6
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-08-26 15:07:02 -0300

    Migrating after create category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826171540) do
+ActiveRecord::Schema.define(version: 20150826173857) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "categories", ["name"], name: "index_categories_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
