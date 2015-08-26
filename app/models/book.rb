8b1aea2355bb38165fd6ea49ec957d4823bb8445
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-08-26 16:36:43 -0300

    Adding validations in the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
+
+  validates :title, :authors, :isbn, :publisher, :published_date, :language, :pages, :category_id, :user_id, presence: true
+  validates :isbn, length: { minimum: 10 }
+  validates :pages, numericality: { only_integer: true, greater_than: 0 }
 end
