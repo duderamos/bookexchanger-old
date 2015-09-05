46c29d95f96ead5dd2ab396467f1f588e22f6857
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-09-05 01:01:57 -0300

    Adding rspec tests for HomeController

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+
+  describe 'GET #index' do
+    it 'returns http success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders "index" template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+end
