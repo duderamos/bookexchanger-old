c4743bdcca1698da22476a0e062a22c06f6208be
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-09-05 00:51:08 -0300

    Adding login scenario

--- spec/features/homepages_spec.rb
+++ spec/features/homepages_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature "Homepages", type: :feature do
+  let(:user) { FactoryGirl.create(:user) }
+
   scenario 'when access homepage' do
     visit root_path
     expect(page).to have_content "Let's spread knowledge!"
   end
   scenario 'when access login page' do
     visit login_path
-    expect(page).to have_content 'Login'
+    expect(page).to have_content 'Log in'
     expect(page).to have_button 'Log in'
+  end
+  scenario 'when user logs in' do
+    visit login_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+    expect(page).to have_content 'Signed in successfully'
   end
 end
