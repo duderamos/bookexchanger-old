3848fc68b2bf1bf690614f506bbd4185fe2adafe
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-09-07 16:21:53 -0300

    Mounting uploader cover to model cover

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,6 @@ class Book < ActiveRecord::Base
   validates :title, :authors, :isbn, :publisher, :published_date, :language, :pages, :category_id, :user_id, presence: true
   validates :isbn, length: { minimum: 10 }
   validates :pages, numericality: { only_integer: true, greater_than: 0 }
+
+  mount_uploader :cover, CoverUploader
 end
