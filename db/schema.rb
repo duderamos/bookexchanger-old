7b888bf1cf131e3ad1b31a629f44a14b9086222f
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-09-07 16:07:33 -0300

    Creating "cover" field to book model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826184828) do
+ActiveRecord::Schema.define(version: 20150907030219) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150826184828) do
     t.integer  "user_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "cover"
   end
 
   add_index "books", ["category_id"], name: "index_books_on_category_id"
