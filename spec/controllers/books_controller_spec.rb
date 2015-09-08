fd80bd42aa3f2c21f3f519ede27258f87cfbcf35
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-09-07 23:26:26 -0300

    Fixing repeated code for DRY

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -10,12 +10,14 @@ RSpec.describe BooksController, type: :controller do
   end
 
   describe 'GET #index' do
-    it 'returns http success' do
+    before :each do
       get :index
+    end
+
+    it 'returns http success' do
       expect(response).to have_http_status :success
     end
     it 'renders "index" template' do
-      get :index
       expect(response).to render_template :index
     end
   end
