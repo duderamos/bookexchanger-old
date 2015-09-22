c248829dca4589b27e8011282cbdb6589cf2c477
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-09-21 22:59:42 -0300

    Enabling I18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def language_selector
+    case I18n.locale
+    when :en
+      link_to 'Português', locale: :"pt-BR"
+    else
+      link_to 'English', locale: :en
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get '/login', to: 'devise/sessions#new'
-    get '/logout', to: 'devise/sessions#destroy'
-  end
-  devise_for :users
-
-  resources :books do
-    member do
-      get 'wish'
+  scope '(:locale)', locale: /en|pt-BR/ do
+    devise_scope :user do
+      get '/login', to: 'devise/sessions#new'
+      get '/logout', to: 'devise/sessions#destroy'
     end
-  end
+    devise_for :users
 
-  root 'home#index'
+    resources :books do
+      member do
+        get 'wish'
+      end
+    end
+
+    root 'home#index'
+  end
 end
